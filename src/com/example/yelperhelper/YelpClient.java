bb3caac2b709e1612f1341e585e71dc10ea5cdcd
Alex Manuel <amanuel@yahoo-inc.com>
2013-10-30 21:48:21 -0700

    Add client access code

---
+++ src/com/example/yelperhelper/YelpClient.java
@@ -0,0 +1,15 @@
+package com.example.yelperhelper;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.loopj.android.http.AsyncHttpClient;
+
+public class YelpClient {
+
+	private static final String BASE_URL = "";
+	private static AsyncHttpClient client = new AsyncHttpClient();
+
+	
+
+}
