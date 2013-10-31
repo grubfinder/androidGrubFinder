73ac0f815555ddda8e3d27b79331263e2ef1b2d6
Alex Manuel <amanuel@yahoo-inc.com>
2013-10-30 23:40:13 -0700

    Stubbed model for a restaurant.  needs parsing logic.

---
+++ src/com/example/yelperhelper/models/Restaurant.java
@@ -0,0 +1,46 @@
+package com.example.yelperhelper.models;
+
+import java.util.List;
+
+public class Restaurant {
+
+	// name
+	private String name;
+
+	// avg_rating
+	private Float avg_rating;
+
+	// review_count
+	private Integer review_count;
+
+	// rating_img_url_small
+	private String rating_img_url_small;
+
+	// phone
+	private String phone;
+
+	// mobile_url
+	private String mobile_url;
+
+	// address1
+	private String address1;
+
+	// address2
+	private String address2;
+
+	// address3
+	private String address3;
+
+	// city
+	private String city;
+
+	// id (yelp id)
+	private String yelpId;
+
+	// photo_url
+	private String photoUrl;
+
+	// categories.name 
+	private List<String> categories;
+
+}
