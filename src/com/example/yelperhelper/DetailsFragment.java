fce224f7564b064341a7b545c78b45c2bcd6a118
Alex Manuel <amanuel@yahoo-inc.com>
2013-11-02 02:50:10 -0700

    rename to use class name in title

--- src/com/example/yelperhelper/DetailsFragment.java
+++ src/com/example/yelperhelper/DetailsFragment.java
@@ -9,7 +9,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class Details extends FragmentActivity {
+public class DetailsFragment extends FragmentActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +21,7 @@ public class Details extends FragmentActivity {
 		btnWantIt.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				Intent i = new Intent(Details.this.getApplicationContext(),
+				Intent i = new Intent(DetailsFragment.this.getApplicationContext(),
 						PictureGridActivity.class);
 				i.putExtra("text", "Passed String Extra!");
 				startActivity(i);
@@ -34,7 +34,7 @@ public class Details extends FragmentActivity {
 		btnNah.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				Intent i = new Intent(Details.this.getApplicationContext(),
+				Intent i = new Intent(DetailsFragment.this.getApplicationContext(),
 						PictureGridActivity.class);
 				i.putExtra("text", "Passed String Extra!");
 				startActivity(i);
